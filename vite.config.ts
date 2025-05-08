47b11117307d9bc2211f7479387381c7b754a877
mfisya <fakhrozan.pp@gmail.com>
2025-05-08 19:22:38 +0700

    edit

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,12 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+    plugins: [sveltekit()],
+    preview: {
+        host: true,        // memungkinkan binding ke 0.0.0.0
+        port: 8070,        // sesuaikan dengan port container Anda
+        allowedHosts: ['deployaja.duckdns.org'],
+    }
 });
+
+
